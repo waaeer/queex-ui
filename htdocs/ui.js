a663e73710fba88fac24679b326600ff90fd8395
Ivan Panchenko <wao@mail.ru>
2021-11-21 03:07:51 +0300

    Can remove dialog element on hide

--- htdocs/ui.js
+++ htdocs/ui.js
@@ -100,6 +100,7 @@ if(!window.qwx) { window.qwx = {} }
 			x.data('isShown', false);
 			x.data('hideInProgress', false);
 			ev.stopPropagation();
+			if(opt.remove) { x.remove(); } 
 		});
 		x.modal(opt);
 	}
